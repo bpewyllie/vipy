a84b4f83bcc5b4d17dd461e5b1eba4cb8800664c
bpewyllie <bpewyllie@utexas.edu>
2021-08-18 23:22:49 -0600

    add playoff indicator column

--- ViPy/bbref.py
+++ ViPy/bbref.py
@@ -78,23 +78,40 @@ def get_season_schedule(season_start_year: int) -> pandas.DataFrame:
         for month in months:
             season_df = season_df.append(get_schedule(season_start_year, month, 1))
 
+    season_df.reset_index(drop=True, inplace=True)
+
     season_df.rename(
         columns={
             "Visitor/Neutral": "away_team",
             "PTS": "away_points",
             "Home/Neutral": "home_team",
             "PTS.1": "home_points",
-            "Unnamed: 7": "overtime",
             "Attend.": "attendance",
             "Notes": "notes",
         },
         inplace=True,
     )
 
-    season_df["start"] = pandas.to_datetime(
-        season_df["Date"] + " " + season_df["Start (ET)"] + "m"
-    )
+    # create indicator for whether game was a playoff game
+    try:
+        playoff_index = season_df.loc[season_df["Date"] == "Playoffs"].index[0]
+        season_df["playoff"] = (season_df.index > playoff_index)
+    except:
+        season_df["playoff"] = None
     
-    season_df.drop(columns={"Date", "Start (ET)", "Unnamed: 6"}, inplace=True)
+    season_df = season_df.loc[season_df["Date"] != "Playoffs"]
 
-    return season_df.reset_index(drop=True)
+    # create start datetime column; old seasons don't have time data
+    season_df["start"] = pandas.to_datetime(
+        season_df["Date"] + " " + season_df.get("Start (ET)", default="")
+    )
+
+    # identify overtime and box score columns based on contents
+
+    # drop unneeded columns
+    # season_df.drop(columns={"Date", "Unnamed: 6"}, inplace=True)
+
+    # if "Start (ET)" in season_df:
+    #     season_df.drop(columns={"Start (ET)"}, inplace=True)
+
+    return season_df
